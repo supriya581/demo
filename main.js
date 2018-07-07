23e5eeb1c8dc4b7fd9fb0162f7a1ec2a99101300
supriya581 <supriyaparupalli98@gmail.com>
2018-07-07 09:27:14 +0530

    add6flie

---
+++ main.js
@@ -0,0 +1,15 @@
+function getfile(file,callback){
+  var xhr = new XMLHttpRequest();
+  xhr.overrideMimeType("application/json");
+  xhr.open("GET",file,true);
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4 && xhr.status == "200"){
+      callback(xhr.responseText);
+    }
+  };
+  xhr.send(null);
+}
+getfile("data.json",function(text){
+  let data =JSON.parse(text);
+  console.log(data);
+})
